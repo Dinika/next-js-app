33d88befe6674004d87178313fb9fc890eb295c2
Dinika <dinikasaxenas@gmail.com>
2019-04-04 02:02:32 +0530

    add greetings as a regular funcitonal component
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

---
+++ components/Greeting.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Greeting = props => <h1>{props.message}</h1>;
+export default Greeting;
--- pages/AnotherPage/index.js
+++ pages/AnotherPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Router from 'next/router';
+import Greeting from '../../components/Greeting';
 
 const AnotherPage = () => (
   <div>
     This is another page <br />
+    <Greeting message="Hola!" />
     <button onClick={() => Router.push('/')}>Back</button>
   </div>
 );
